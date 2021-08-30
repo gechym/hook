2c821752e00b712724964cc0b0c160c85f5b7a17
gechym <nguyenducbao166@gmail.com>
2021-08-30 10:28:07 +0700

    create new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    
+    this.state = {
+      'isCollapsed' : true
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+       Hello word
+      </div>
+    )
+  }
+
 }
+
 
 export default App;
