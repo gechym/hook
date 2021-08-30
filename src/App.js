bdda7518b6f309c1dc4c5f26979795b13125bfb4
gechym <nguyenducbao166@gmail.com>
2021-08-30 19:27:36 +0700

    useMemo - how to use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import Example from './components/Example'
 
+
+let expensiveFunction = (number) => { // hàm này mô phỏng một hàm xử lý phức tạp 
+  console.log('bất đầu')
+  let start = new Date()
+
+  while(new Date() - start < 3000);
+
+  console.log('kết thúc', + new Date() - start ,'ms')
+
+  return number * number
+
+  // khi component re-render
+  //thì phải gọi lại hàm đấy 
+  //như giá trị truyền vào 
+  //vẫn không thay đổi 
+}
+
 function App(){
+  const [count , setCount] = useState(0)
+  const [num , setNum] = useState(20)
+
+  // useMemo sử dụng kỷ thuật lưu case khi khá trị đó truyền vào không đổi thì nó sẻ trả về nhanh hơn 
+  let number = useMemo(()=> {
+    return expensiveFunction(num)
+  },[num]) // hàm này nhận 2 tham số 1 fn vs một array nếu để [] thì nó chỉ render lại một lần duy nhất
+  
   return(
     <>
-      hello world
-      <Example></Example>     
+      <h1>useMemo </h1>
+      <p>you click {count} times</p>
+      <button onClick={() => {setCount(count + 1)}}>Click me</button>
+      <button onClick={() => {setNum(num + 1)}}>Click me to change value num</button>
+
+      <p>Number {number}</p>
     </>
   )
 }
--- src/components/Example.js
+++ src/components/Example.js
@@ -1,52 +1,9 @@
 import React , { useState , useEffect } from 'react'
 
 function Example(){
-    const [count, setCount] = useState(0)
-    const [action, setAction] = useState('')
-    const [scrollPosition , setScrollPosition] = useState(0)
-
-    useEffect(()=>{
-        // useEffect này đóng vai trò như một componetdidMount & componentDidUpdate 
-
-        document.title = `you click ${count} times`
-        console.log('component re-render')
-    }, [count]) // tham số thứ 2 của useEffect khi truyền vào một array có các phần tử là cái biến của state 
-        // khi đó nó componet sẽ được render lại nến biến state của nó thay đổi ... 
-
-
-
-
-    useEffect(()=>{
-        fetch(`https://reqres.in/api/${action}`)
-            .then((res) => {console.log(res)})
-            .catch((err) => {console.log(err)})
-    }, [action]) 
-
-    let handleScroll = () => {
-        setScrollPosition(window.scrollY)
-    }
-
-    useEffect(()=>{
-        document.addEventListener('scroll', handleScroll)
-
-        return () => {
-            document.removeEventListener('scroll', handleScroll)
-        }
-    }, []) 
-
     
-    
-    
-    console.log('component mount')
     return(
-        <div style={{height: '3000px'}}>
-            <h4>useEffect</h4>
-            <p>you click {count} times</p>
-            <p style={{position: 'fixed', top : '50%' }}> value y : {scrollPosition}</p>
-            <button onClick={()=>{setCount(count + 1)}}>Click me</button>
-            <button onClick={()=>{setAction('users')}}>get users</button>
-            <button onClick={()=>{setAction('comments')}}>get comments</button>
-        </div>
+       <div>Hello world</div>
     )
 }
 
